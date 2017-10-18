95112a4fa91ab624e42a6dceba9470e3d8765e59
Bart Jimenez Vera <bartolomehjvera@gmail.com>
2017-10-18 22:49:21 +0200

    Refactored code, using printf and formats instead of Echo, outputing plain and HTML

--- portfolio.sh
+++ portfolio.sh
@@ -4,57 +4,114 @@
 
 ##### Constants
 
-TITLE="System Information for $HOSTNAME"
+TITLE="System Information requested by $HOSTNAME"
 RIGHT_NOW=$(date +"%x %r %Z")
 TIME_STAMP="Updated on $RIGHT_NOW by $USER"
-
+FOOTER="@bart @carlos | SDU Robotics | Linux for Embedded Systems"
+is_plain=1
+GENERAL_FORMAT="\n%40s\n\n%35s\n%s\n"
+DIVIDER=============================
+DIVIDER=$DIVIDER$DIVIDER
+DIVIDER_WIDTH=60
+DIVIDER_FORMAT="\n%$DIVIDER_WIDTH.${DIVIDER_WIDTH}s\n"
 ##### Functions
 
 system_info()
 {
-    echo "<h2>System release info</h2>"
-    echo "<p>Function not yet implemented</p>"
-
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "System release info" "Function not yet implemented" ""
+  else
+    printf "$GENERAL_FORMAT" "<h2>System release info</h2>" "<p>Function not yet implemented</p>" ""
+  fi
 }   # end of system_info
 
-
+users_connected()
+{
+  aux="$(who -q | head -n -1)"
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "List of logged in users" "$aux" ""
+  else
+    printf "$GENERAL_FORMAT" "<h2>List of logged in users</h2><pre>" "$aux" "</pre>"
+  fi
+}
 show_uptime()
 {
-    echo "<h2>System uptime</h2>"
-    echo "<pre>"
-    uptime
-    echo "</pre>"
-
+  aux="$(uptime -p)"
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "System Uptime" "$aux" ""
+  else
+    printf "$GENERAL_FORMAT" "<h2>System uptime</h2><pre>" "$aux" "</pre>"
+  fi
 }   # end of show_uptime
 
 
 drive_space()
 {
-    echo "<h2>Filesystem space</h2>"
-    echo "<pre>"
-    df
-    echo "</pre>"
-
+  aux="$(df -h)"
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "Filesystem space" "$aux" ""
+  else
+    printf "$GENERAL_FORMAT" "<h2>Filesystem space</h2><pre>" "$aux" "</pre>"
+  fi
 }   # end of drive_space
 
+ram_space()
+{
+  aux="$(free -m)"
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "RAM consumption" "$aux" ""
+  else
+    printf "$GENERAL_FORMAT" "<h2>RAM consumption</h2><pre>" "$aux" "</pre>"
+  fi
+}   # end of ram_space
+
+footer()
+{
+  if [ "$is_plain" -eq 0 ]; then
+    printf "$DIVIDER_FORMAT" "$DIVIDER"
+    printf "$GENERAL_FORMAT" "ABOUT" "$FOOTER" ""
+  else
+    printf "$GENERAL_FORMAT" "<footer><pre>" "$FOOTER" "</pre></footer>"
+  fi
+}
 
 home_space()
 {
-    # Only the superuser can get this information
-
-    if [ "$(id -u)" = "0" ]; then
-        echo "<h2>Home directory space by user</h2>"
-        echo "<pre>"
-        echo "Bytes Directory"
-        du -s /home/* | sort -nr
-        echo "</pre>"
-    fi
-
+  # Only the superuser can get this information
+  aux="$(du -s /home/* | sort -nr)"
+  if [ "$(id -u)" = "0" ]; then
+    if [ "$is_plain" -eq 0 ]; then
+      printf "$DIVIDER_FORMAT" "$DIVIDER"
+      printf "$GENERAL_FORMAT" "Home directory space by user" "$aux" ""
+    else
+      printf "$GENERAL_FORMAT" "<h2>Home directory space by user</h2><pre>Bytes Directory" "$aux" "</pre>"
+    fi   
+  fi
 }   # end of home_space
 
 write_page()
 {
+
+  if [ "$is_plain" -eq 0 ]; then
+  cat <<- _EOF2_
+  $TITLE
+  $TIME_STAMP
+  $(system_info)
+  $(users_connected)
+  $(show_uptime)
+  $(drive_space)
+  $(ram_space)
+  $(home_space)
+  $(footer)
+_EOF2_
+  else
     cat <<- _EOF_
+    <!DOCTYPE html>
     <html>
         <head>
         <title>$TITLE</title>
@@ -63,18 +120,21 @@ write_page()
         <h1>$TITLE</h1>
         <p>$TIME_STAMP</p>
         $(system_info)
+        $(users_connected)
         $(show_uptime)
         $(drive_space)
+        $(ram_space)
         $(home_space)
+        $(footer)
         </body>
-    </html>
+      </html> 
 _EOF_
-
+  fi
 }
 
 usage()
 {
-  echo "Usage: portfolio.sh [OPTION]..."
+  echo "Usage: portfolio.sh [OPTION]..." #export PATH=$PATH:$(pwd)
   echo "Generates an output containing system information such as date, users and more." 
   echo "If no option is given, output is generated in HTML format."
   echo
@@ -85,69 +145,17 @@ usage()
   echo 
 }
 
-
-DSU="Disk Space Utilization"
-mu="Memory Usage"
-hsu="Home Space Utilization"
-cale="Month calendar"
-bottom="@bart @carlos | SDU Robotics | Linux for Embedded Systems"
-CURRENT_TIME=$(date)
-TIMESTAMP=$(uptime)
-opt="?"
-place="Odense | Funen | Denmark"
-
-while getopts ":ha" opt; do
+while getopts ":hp" opt; do
 	case $opt in
-	       	h)
-			usage
-			exit 0
-			;;
-		a)
-			echo $TITLE | od -An -vtu1
-			echo "dare" | od -An -vtu1
-			date | od -An -vtu1
-			echo "Uptime and load average:" | od -An -vtu1
-			uptime | od -An -vtu1
-			echo "Logged-in users:" | od -An -vtu1
-			who | od -An -vtu1
-			echo "RAM memory usage:" | od -An -vtu1
-			df -h | od -An -vtu1
-			echo "Amount of free disk space" | od -An -vtu1
-			free -m | od -An -vtu1
-			echo "Month calendar" | od -An -vtu1
-			cal | od -An -vtu1
-			echo $bottom | od -An -vtu1
-			exit 0
-			;;
+      h)
+  			usage
+  			exit 0
+  		  ;;
+      p)is_plain=0
+        ;;
 	esac
 done
 
 
 
-
-
-if [ "$opt" = "?" ]; then
-echo "
-<HTML>
-	<HEAD>
-		<TITLE>$TITLE</TITLE>
-	</HEAD>
-	<BODY text = "orange" bgcolor = "black">
-		<H1 align = "center">$title</H1>
-		<hr />
-		<P>$CURRENT_TIME<P>
-		<P>$TIMESTAMP</P>
-		<P>$place</P>
-		<H2>$dsu</H2>
-		<PRE>$(df -h)</PRE>
-		<H2>$mu</H2>
-		<PRE>$(free -m)</PRE>
-		<H2>$hsu</H2>
-		<PRE>$(du -sh /home/*)</PRE>
-		<H2>$cale</H2>
-		<PRE>$(cal)</PRE>
-		<hr />
-		<H3 align = "center">$bottom</H3>
-	</BODY>
-</HTML>"
-fi
+write_page
